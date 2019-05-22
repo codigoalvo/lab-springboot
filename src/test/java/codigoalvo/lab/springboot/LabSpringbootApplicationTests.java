82969aa7a9caa2d84b5628a126e15743125e916c
Cassio Reinaldo Amaral <cassio.amaral@gmail.com>
2019-05-22 15:11:04 -0300

    Esqueleto do projeto e arquivos de configuração das IDEs

---
+++ src/test/java/codigoalvo/lab/springboot/LabSpringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package codigoalvo.lab.springboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LabSpringbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
